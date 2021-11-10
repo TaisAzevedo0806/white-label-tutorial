805269f4a4e5d1aee95189c6e57b0357b09d6c0e
TaisAzevedo0806 <taisazevedo0806@hotmail.com>
2021-11-10 16:11:24 -0300

    #5 Finalizando a implementação do FirebaseDataSource

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
@@ -5,9 +5,11 @@ import br.com.douglasmotta.whitelabeltutorial.BuildConfig
 import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
 import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_PRODUCTS
 import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_ROOT
+import br.com.douglasmotta.whitelabeltutorial.util.STORAGE_IMAGES
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 import kotlin.coroutines.suspendCoroutine
 
 class FirebaseProductDataSource(
@@ -37,30 +39,68 @@ class FirebaseProductDataSource(
 
             val productsReference = documentReference.collection(COLLECTION_PRODUCTS)
 
-            productsReference.get().addOnSuccessListener { documents ->
-                val products = mutableListOf<Product>()
+            productsReference.get()
+                .addOnSuccessListener { documents ->
 
-                for (document in documents) {
-                    document.toObject(Product::class.java).run {
-                        products.add(this)
+                    val products = mutableListOf<Product>()
+
+                    for (document in documents) {
+                        document.toObject(Product::class.java).run {
+                            products.add(this)
+                        }
                     }
+
+                    continuation.resumeWith(Result.success(products))
+
                 }
-
-                continuation.resumeWith(Result.success(products))
-            }
-
-            productsReference.get().addOnFailureListener { exception ->
-                continuation.resumeWith(Result.failure(exception))
-            }
+                .addOnFailureListener { exception ->
+                    continuation.resumeWith(Result.failure(exception))
+                }
         }
     }
 
+    /**
+     * Firebase's storage structure:
+     *
+     * images/car/randomKey
+     * images/bike/randomKey
+     *
+     */
+
     override suspend fun uploadProductImage(imageUri: Uri): String {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+
+            val randomKey = UUID.randomUUID()
+            val childReference =
+                storageReference.child("$STORAGE_IMAGES/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/$randomKey")
+
+            childReference.putFile(imageUri)
+                .addOnSuccessListener { taskSnapshot ->
+
+                    taskSnapshot.storage.downloadUrl.addOnSuccessListener { uri ->
+                        continuation.resumeWith(Result.success(uri.toString()))
+                    }
+
+                }
+                .addOnFailureListener { exception ->
+                    continuation.resumeWith(Result.failure(exception))
+                }
+
+        }
     }
 
     override suspend fun createProduct(product: Product): Product {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            documentReference
+                .collection(COLLECTION_PRODUCTS)
+                .document(System.currentTimeMillis().toString())
+                .set(product)
+                .addOnSuccessListener {
+                    continuation.resumeWith(Result.success(product))
+                }
+                .addOnFailureListener { exception ->
+                    continuation.resumeWith(Result.failure(exception))
+                }
+        }
     }
-
 }
