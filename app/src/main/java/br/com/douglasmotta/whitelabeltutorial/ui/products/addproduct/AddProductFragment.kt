4a98fb2608f74fdb8a7d6e802209b3eb4a9e3c6f
TaisAzevedo0806 <taisazevedo0806@hotmail.com>
2021-11-11 13:58:29 -0300

    #8 Dando vida ao formulário e seleção de imagem

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/addproduct/AddProductFragment.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/addproduct/AddProductFragment.kt
@@ -1,10 +1,13 @@
 package br.com.douglasmotta.whitelabeltutorial.ui.products.addproduct
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import br.com.douglasmotta.whitelabeltutorial.databinding.AddProductFragmentBinding
+import br.com.douglasmotta.whitelabeltutorial.util.CurrencyTextWatcher
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class AddProductFragment : BottomSheetDialogFragment() {
@@ -14,9 +17,36 @@ class AddProductFragment : BottomSheetDialogFragment() {
 
     private lateinit var viewModel: AddProductViewModel
 
+    private var imageUri: Uri? = null
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        imageUri = uri
+        binding.imageProduct.setImageURI(uri)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = AddProductFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.imageProduct.setOnClickListener { pickImage() }
+        binding.buttonAddProduct.setOnClickListener { addProduct() }
+        binding.inputPrice.run { addTextChangedListener(CurrencyTextWatcher(this)) }
+    }
+
+    private fun pickImage() {
+        getContent.launch("image/*")
+    }
+
+    private fun addProduct() {
+        val description = binding.inputDescription.text.toString()
+        val price = binding.inputPrice.text.toString()
     }
 
 }
