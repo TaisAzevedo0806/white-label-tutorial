3c63a29295a594769f67c1f5a50ca26cd118c5c8
TaisAzevedo0806 <taisazevedo0806@hotmail.com>
2021-11-12 16:24:47 -0300

    #9 Implementando o ViewModel e Validando o Formulário

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/addproduct/AddProductFragment.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/addproduct/AddProductFragment.kt
@@ -9,6 +9,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import br.com.douglasmotta.whitelabeltutorial.databinding.AddProductFragmentBinding
 import br.com.douglasmotta.whitelabeltutorial.util.CurrencyTextWatcher
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.textfield.TextInputLayout
 
 class AddProductFragment : BottomSheetDialogFragment() {
 
@@ -31,7 +32,15 @@ class AddProductFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        observeVMEvents()
         setListeners()
+    }
+
+    private fun observeVMEvents() {
+        viewModel.imageUriErrorResId.observe(viewLifecycleOwner) { drawableResId -> binding.imageProduct.setBackgroundResource(drawableResId) }
+        viewModel.descriptionFieldErrorResId.observe(viewLifecycleOwner) { stringResId -> binding.inputLayoutDescription.setError(stringResId) }
+        viewModel.priceFieldErrorResId.observe(viewLifecycleOwner) { stringResId -> binding.inputLayoutPrice.setError(stringResId) }
     }
 
     private fun setListeners() {
@@ -47,6 +56,12 @@ class AddProductFragment : BottomSheetDialogFragment() {
     private fun addProduct() {
         val description = binding.inputDescription.text.toString()
         val price = binding.inputPrice.text.toString()
+
+        viewModel.createProduct(description, price, imageUri)
+    }
+
+    private fun TextInputLayout.setError(stringResId: Int?) {
+        error = if (stringResId != null) getString(stringResId) else null
     }
 
 }
